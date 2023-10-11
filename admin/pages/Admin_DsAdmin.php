79bd9a485eb6847e775f6f0094ee816ffa1c299d
Lam Ngô <ngotuanlam147@hotmail.com>
2023-10-11 18:03:12 +0700

    Admin

--- admin/pages/Admin_DsAdmin.php
+++ admin/pages/Admin_DsAdmin.php
@@ -1,47 +1,113 @@
 <?php include '../templates/nav_admin1.php' ?>
-<div class="detail_admin">
-    <h1 class="Title_Admin_create_form">Thông tin tài khoản</h1>
-    <div class="detai_admin_form">
-        <div class="detail_admin_left">
-            <img src="~/assest/img/ad_user/@Model.AVATAR" alt="">
-        </div>
-        <div class="detail_admin_right">
-            <table class="Table_Details_Admin">
-                <tr>
-                    <td>Mã Admin: </td>
-                    <td>@Model.MAADMIN</td>
-                </tr>
-                <tr>
-                    <td>Họ tên Admin:</td>
-                    <td>@Model.HOTEN</td>
-                </tr>
-                <tr>
-                    <td>Địa chỉ:</td>
-                    <td>@Model.DIACHI</td>
-                </tr>
-                <tr>
-                    <td>Số điện thoại :</td>
-                    <td>@Model.DIENTHOAI</td>
-                </tr>
-                <tr>
-                    <td>Email :</td>
-                    <td>@Model.EMAIL</td>
-                </tr>
-                <tr>
-                    <td>Loại tài khoản :</td>
-                    <td>@Model.LOAITKADMIN.TENLOAI</td>
-                </tr>
-            </table>
-
-        </div>
-
+<div class="table_header">
+    <div class="search">
+        <a href="@Url.Action("Search","Admin")">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <div class="search_title">
+                Tìm kiếm nhanh
+            </div>
+        </a>
     </div>
-    <div class="button">
-        <a href="@Url.Action(" Edit","Admin",new {id=Model.MAADMIN})"><input type="submit" value="Chỉnh sửa"
-                class="button_add_admin" /></a>
-        <a href="@Url.Action(" DSAdmin","Admin")"><input type="button" value="Quay lại" class="button_add_admin" /></a>
+    <div class="add_admin">
+        <a href="@Url.Action("Create","Admin")">
+            <i class="fa-solid fa-user-plus"></i>
+            <div class="add_title">
+                Thêm Admin
+            </div>
+        </a>
     </div>
-
 </div>
+<table class="table_dsadmin">
+    <thead>
+        <tr>
+            <th style="width: 65px;">Mã Admin</th>
+            <th style="width: 120px;">Họ tên Admin</th>
+            <th style="width: 150px;">Địa chỉ</th>
+            <th style="width: 80px;">Số điện thoại</th>
+            <th style="width: 90px;">Loại tài khoản</th>
+            <th style="width: 100px;">Tên đăng nhập</th>
+            <th style="width: 75px;">Hình ảnh</th>
+            <th style="width: 150px;">Email</th>
+            <th style="width: 80px;">Chức năng</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach (var item in Model)
+        {
+            <tr>
+                <td>
+                    @Html.DisplayFor(modelItem => item.MAADMIN)
+                </td>
+                <td>
+                    @Html.DisplayFor(modelItem => item.HOTEN)
+                </td>
+                <td>
+                    @Html.DisplayFor(modelItem => item.DIACHI)
+                </td>
+                <td>
+                    @Html.DisplayFor(modelItem => item.DIENTHOAI)
+                </td>
+                <td>
+                    @Html.DisplayFor(modelItem => item.LOAITKADMIN.TENLOAI)
+                </td>
+                <td>
+                    @Html.DisplayFor(modelItem => item.TENDN)
+                </td>
+                <td>
+                    <img src="@Url.Content("~/assest/img/ad_user/" + item.AVATAR)" alt="" style="width: 70px; height: 70px;">
+                </td>
+                <td>
+                    @Html.DisplayFor(modelItem => item.EMAIL)
+                </td>
+                <td>
+                    <a href="@Url.Action("Edit","Admin", new { id = item.MAADMIN })"><i class="fa-solid fa-pen-to-square edit"></i></a>
+                    <a href="@Url.Action("Delete","Admin", new { id = item.MAADMIN })"> <i class="fa-solid fa-xmark remove"></i></a>
+                    <a href="@Url.Action("Detail","Admin", new { id = item.MAADMIN })"><i class="fa-solid fa-circle-info detail"></i></a>
+                </td>
+            </tr>
+        }
+
+
+    </tbody>
+</table>
+@*<div align="center">@Html.PagedListPager(Model, page => Url.Action("DSAdmin", new { page = page }))</div>*@
+
+<ul class="page">
+    @if (Model.PageCount > 1)
+    {
+        for (int i = 1; i <= Model.PageCount; i++)
+        {
+            <li>
+                <a href="@Url.Action("DSAdmin", new { page = i })" class="@((i == Model.PageNumber) ? "page_button page_button_active" : "page_button")">@i</a>
+            </li>
+        }
+    }
+</ul>
+@*
+
+    <div class="page">
+         <div class="page_previous_button">
+             <i class="fa-solid fa-angles-left"></i>
+         </div>
+         <div class="page_button page_button_active">
+             1
+         </div>
+         <div class="page_button">
+             2
+         </div>
+         <div class="page_button">
+             3
+         </div>
+         <div class="page_button">
+             4
+         </div>
+         <div class="page_button">
+             5
+         </div>
+         <div class="page_behind_button">
+             <i class="fa-solid fa-angles-right"></i>
+         </div>
+     </div>
+*@ 
 
 <?php include '../templates/nav_admin2.php' ?>
