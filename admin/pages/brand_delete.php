606ae3ecf5b9ee20c20520102a40cf1ea0602bd4
huuluc10 <lucnguyenhuu91@gmail.com>
2023-10-30 07:55:39 +0700

    Thêm kiểm tra xóa

--- admin/pages/brand_delete.php
+++ admin/pages/brand_delete.php
@@ -7,8 +7,21 @@ $statement->execute();
 $statement->setFetchMode(PDO::FETCH_OBJ);
 $result = $statement->fetch();
 if (isset($_POST["delete"])) {
-    $statement = $dbh->prepare("DELETE FROM `thuong_hieu` WHERE `maThuongHieu` = '" . $brand_id . "'");
+    $statement = $dbh->prepare("SELECT * FROM `san_pham` WHERE `maThuongHieu` = '" . $brand_id . "'");
     $statement->execute();
+    $statement->setFetchMode(PDO::FETCH_OBJ);
+    $result = $statement->fetch();
+
+    if ($statement->rowCount() == 0) {
+        $statement = $dbh->prepare("DELETE FROM `thương_hieu` WHERE `maThuongHieu` = '" . $brand_id . "'");
+        $statement->execute();
+
+    } else {
+        echo "<script>
+            alert(\"Thuong hiệu đang có sản phẩm kinh doanh\");
+        </script>";
+
+    }
     echo '<script>window.location.href = "brand_index.php";</script>';
 }
 ?>
