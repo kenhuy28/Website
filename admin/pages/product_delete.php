fe287fc06c07d487a0e92976cd75f8a4324fdf5e
huuluc10 <lucnguyenhuu91@gmail.com>
2023-10-31 16:23:54 +0700

    index và delete

--- admin/pages/product_delete.php
+++ admin/pages/product_delete.php
@@ -1,45 +1,48 @@
-<?php include '../templates/nav_admin1.php' ?>
+<?php
+include '../templates/nav_admin1.php';
+include '../includes/get_product_data_from_id.php';
+include '../includes/delete_product_from_id.php';
+?>
 <div class="body" style="margin-top: 15px">
     <div class="detail_admin">
         <h1 class="Title_Admin_create_form">Bạn có muốn xóa sản phẩm này ?</h1>
         <form action="" method="post">
             <div class="detai_admin_form">
                 <div class="detail_admin_left">
-                    <img src="https://webkit.org/demos/srcset/image-src.png" alt="">
+                    <img src="<?php echo $_SESSION['rootPath'] . '/../assets/img/sanpham/' . $result->hinhAnh; ?>"
+                        alt="">
                 </div>
                 <div class="detail_admin_right">
                     <table class="Table_Details_Admin">
                         <tr>
                             <td>Mã sản phẩm: </td>
-                            <td>@Model.MASP</td>
+                            <td>
+                                <?php echo $result->maSanPham; ?>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tên sản phẩm:</td>
-                            <td>@Model.TENSP</td>
-                        </tr>
-                        <tr>
-                            <td>Đơn giá mua:</td>
-                            <td>@Model.DONGIAMUA</td>
-                        </tr>
-                        <tr>
-                            <td>Đơn giá bán :</td>
-                            <td>@Model.DONGIABAN</td>
+                            <td>
+                                <?php echo $result->tenSanPham; ?>
+                            </td>
                         </tr>
                         <tr>
                             <td>Thương hiệu :</td>
-                            <td>@Model.THUONGHIEU.TENTH</td>
+                            <td>
+                                <?php echo $result->tenThuongHieu; ?>
+                            </td>
                         </tr>
                         <tr>
                             <td>Loại sản phẩm :</td>
-                            <td>@Model.LOAI.TENLOAI</td>
-                        </tr>
-                        <tr>
-                            <td>Số lượng :</td>
-                            <td>@Model.SOLUONG</td>
+                            <td>
+                                <?php echo $result->tenLoai; ?>
+                            </td>
                         </tr>
                         <tr>
                             <td>Mô tả :</td>
-                            <td style="max-width: 200px;">@Model.MOTA</td>
+                            <td style="max-width: 200px; text-align: justify;">
+                                <?php echo $result->moTa; ?>
+                            </td>
                         </tr>
                     </table>
 
@@ -47,13 +50,14 @@
 
             </div>
             <div class="button">
-                <input type="button" value="Xóa" class="button_add_admin delete_display_alert" />
-                <a href="product_admin_index.php"><input type="button" value="Quay lại" class="button_add_admin" /></a>
+                <input type="button" value="Xóa" class="button_add_admin delete_display_alert"  />
+                <a href="javascript:history.go(-1);"><input type="button" value="Quay lại"
+                        class="button_add_admin" /></a>
             </div>
             <div class="alert_delete">
-                <div class="notification">
+                <div class="notification" style="width:20%">
                     <h1 class="notification_title">Xác nhận xóa sản phẩm này!</h1>
-                    <input type="submit" value="Xóa" class="alert_delete_btn delete_conform" />
+                    <input type="submit" value="Xóa" class="alert_delete_btn delete_conform" name="delete"/>
                     <input type="button" value="Không" class="alert_delete_btn delete_cancel" />
                 </div>
             </div>
