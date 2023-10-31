c8d79a10da469db990b628a1af33cda2d1370501
huuluc10 <lucnguyenhuu91@gmail.com>
2023-10-31 21:12:53 +0700

    tách xử lý khỏi giao diện

--- admin/pages/type_delete.php
+++ admin/pages/type_delete.php
@@ -1,30 +1,8 @@
 <?php
 include '../templates/nav_admin1.php';
-$type_id = $_GET['id'];
+include '../includes/get_type_data_from_id.php';
+include '../includes/delete_type_from_id.php';
 
-$statement = $dbh->prepare("SELECT * FROM `loai_san_pham` WHERE `maLoai` = '" . $type_id . "'");
-$statement->execute();
-$statement->setFetchMode(PDO::FETCH_OBJ);
-$result = $statement->fetch();
-
-if (isset($_POST["delete"])) {
-    $statement = $dbh->prepare("SELECT * FROM `san_pham` WHERE `maLoai` = '" . $type_id . "'");
-    $statement->execute();
-    $statement->setFetchMode(PDO::FETCH_OBJ);
-    $result = $statement->fetch();
-
-    if ($statement->rowCount() == 0) {
-        $statement = $dbh->prepare("DELETE FROM `loai_san_pham` WHERE `maLoai` = '" . $type_id . "'");
-        $statement->execute();
-
-    } else {
-        echo "<script>
-            alert(\"Loại sản phẩm này đang kinh doanh\");
-        </script>";
-
-    }
-    echo '<script>window.location.href = "type_index.php";</script>';
-}
 ?>
 
 <div class="detail_admin">
