1ab74735dad1a024febe27c4bc3b7e5108c04caa
Lam Ngô <ngotuanlam147@hotmail.com>
2023-11-01 18:06:49 +0700

    dndk

--- pages/login.php
+++ pages/login.php
@@ -1,9 +1,46 @@
 <!--login-->
-<?php include '../templates/header.php' ?>
+<?php include '../templates/header.php';
+$conn = mysqli_connect('localhost', 'root', '', 'qlpkthucung');
+mysqli_set_charset($conn, 'UTF8');
+$sql = "SELECT *
+        FROM khach_hang
+        ";
+$rerult = mysqli_query($conn, $sql);
+
+if (isset($_POST["dangNhap"])) {
+    $check = 1;
+    if ($_POST['tendn'] == NULL) {
+        echo 'Vui lòng nhập tài khoản <br>';
+        $check = 2;
+    } else {
+        $username = $_POST['tendn'];
+    }
+    if ($_POST['matkhau'] == NULL) {
+        echo 'Vui lòng nhập mật khẩu';
+        $check = 2;
+    } else {
+        $password = $_POST['matkhau'];
+    }
+
+    while ($row = mysqli_fetch_array($rerult)) {
+        if ($_POST['tendn'] == $row[5] && $_POST['matkhau'] == $row[6]) {
+            echo '<script>window.location.href = "../index.php?";</script>';
+            $_SESSION['maKhachHang'] = $row[0];
+            $check = 2;
+        }
+    }
+    if ($check == 1) {
+        echo 'Thông tin không chính xác. Vui lòng đăng nhập lại';
+    }
+}
+
+
+?>
+
 <div class="login_flex_right_title">
     <h6>Đăng Nhập</h6>
 </div>
-<form action="" class="create_admin_form">
+<form action="" method="POST" class="create_admin_form">
     <div class="form_field">
         <label for="" class="name_form_field">Tài khoản : </label>
         <input type="text" class="textfile" name="tendn" id="taikhoan">
@@ -14,10 +51,10 @@
         <input type="password" class="textfile" id="matkhau" name="matkhau">
         <span class="error_message"></span>
     </div>
-    <input type="submit" value="Đăng Nhập" class="button_add_admin" style="width: 150px" />
+    <input type="submit" name="dangNhap" value="Đăng Nhập" class="button_add_admin" style="width: 150px" />
     <a href="" class="qmk" style="display: block; margin-left: 10px; color: black">Quên
         mật khẩu?</a>
-    <a href="" style="text-decoration: none; color:black">
+    <a href="./register.php" style="text-decoration: none; color:black">
         <input type="button" value="Tạo Tài Khoản" class="button_add_admin" style="width: 150px; margin-bottom: 50px" />
     </a>
 </form>
--- pages/register.php
+++ pages/register.php
@@ -1,62 +1,170 @@
 <!--register-->
 
-<?php include '../templates/header.php' ?>
+<?php
+include '../templates/header.php';
+$conn = mysqli_connect('localhost', 'root', '', 'qlpkthucung');
+mysqli_set_charset($conn, 'UTF8');
+
+$maKhachHanh = "KH";
+$query = "SELECT COUNT(*) FROM `phieu_nhap`";
+$statement = $dbh->prepare($query);
+$statement->execute();
+// Sử dụng fetchColumn
+$count = $statement->fetchColumn() + 2;
+if ($count <= 9) {
+    $maKhachHanh .= "000" . $count;
+} else if ($count <= 99) {
+    $maKhachHanh .= "00" . $count;
+} else if ($count <= 999) {
+    $maKhachHanh .= "0" . $count;
+} else {
+    $maKhachHanh .= "" . $count;
+}
+if (!empty($_POST['hoKhachHang'])) {
+    $hoKhachHang = $_POST['hoKhachHang'];
+} else
+    $hoKhachHang = "";
+if (!empty($_POST['tenKhachHang'])) {
+    $tenKhachHang = $_POST['tenKhachHang'];
+} else
+    $tenKhachHang = "";
+if (!empty($_POST['dienThoai'])) {
+    $dienThoai = $_POST['dienThoai'];
+} else
+    $dienThoai = "";
+if (!empty($_POST['diaChi'])) {
+    $diaChi = $_POST['diaChi'];
+} else
+    $diaChi = "";
+if (!empty($_POST['tendn'])) {
+    $tendn = $_POST['tendn'];
+} else
+    $tendn = "";
+if (!empty($_POST['matKhau'])) {
+    $matKhau = $_POST['matKhau'];
+} else
+    $matKhau = "";
+if (!empty($_POST['email'])) {
+    $email = $_POST['email'];
+} else
+    $email = "";
+if (!empty($_POST['ngaySinh'])) {
+    $ngaySinh = $_POST['ngaySinh'];
+} else
+    $ngaySinh = "";
+$fileUpLoad = "123.jpg";
+$khoiPhuc = "123456";
+$maXa = "X00011";
+if (isset($_POST["submit"])) {
+
+    $sql_themKhachHang = "INSERT INTO khach_hang VALUES ('$maKhachHanh','$hoKhachHang','$tenKhachHang','$dienThoai','$diaChi','$tendn','$matKhau','$email','$ngaySinh','$fileUpLoad','$khoiPhuc','$maXa');";
+    $rerult_themKhachHang = mysqli_query($conn, $sql_themKhachHang);
+}
+?>
+
 <h6>Trang chủ > Đăng ký </h6>
+<script>
+    fetch('https://vn-public-apis.fpo.vn/provinces/getAll?limit=-1')
+        .then(response => response.json())
+        .then(data => {
+            let provinces = data.data.data;
+            provinces.map(value => document.getElementById('provinces').innerHTML += `<option value='${value.code}'>${value.name}</option>`);
+        })
+        .catch(error => {
+            console.error('Lỗi khi gọi API:', error);
+        });
+
+    function fetchDistricts(provincesID) {
+        fetch(`https://vn-public-apis.fpo.vn/districts/getByProvince?provinceCode=${provincesID}&limit=-1`)
+            .then(response => response.json())
+            .then(data => {
+                let districts = data.data.data;
+                if (districts !== undefined) {
+                    districts.map(value => document.getElementById('districts').innerHTML += `<option value='${value.code}'>${value.name}</option>`);
+                }
+            })
+            .catch(error => {
+                console.error('Lỗi khi gọi API:', error);
+            });
+    }
+
+    function fetchWards(districtsID) {
+        fetch(`https://vn-public-apis.fpo.vn/wards/getByDistrict?districtCode=${districtsID}&limit=-1`)
+            .then(response => response.json())
+            .then(data => {
+                let wards = data.data.data;
+                if (wards !== undefined) {
+                    wards.map(value => document.getElementById('wards').innerHTML += `<option value='${value.code}'>${value.name}</option>`);
+                }
+            })
+            .catch(error => {
+                console.error('Lỗi khi gọi API:', error);
+            });
+    }
+
+    function getProvinces(event) {
+        fetchDistricts(event.target.value);
+    }
+
+    function getDistricts(event) {
+        fetchWards(event.target.value);
+    }
+
+</script>
 <div class="create_admin">
     <h1 class="Title_Admin_create_form">Tạo tài khoản </h1>
     <p class="Notification_create_form">Vui lòng điền thông tin bên dưới</p>
-    <form class="create_admin_form">
+    <form class="create_admin_form" method="POST">
         <div class="form_field">
-            <label for="" class="name_form_field">Họ tên: </label>
-            <input type="text" class="textfile" id="fullname" name="HOTENKH" style="width: 400px;">
+            <label for="" class="name_form_field">Họ khách hàng: </label>
+            <input type="text" class="textfile" name="hoKhachHang" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
-
+        <div class="form_field">
+            <label for="" class="name_form_field">Tên khách hàng: </label>
+            <input type="text" class="textfile" name="tenKhachHang" style="width: 400px;" required>
+            <span class="error_message"></span>
+        </div>
         <div class="form_field">
             <label for="" class="name_form_field">Số điện thoại : </label>
-            <input type="text" class="textfile" id="phoneNumber" name="DIENTHOAI" style="width: 400px;">
+            <input type="text" class="textfile" id="phoneNumber" name="dienThoai" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div class="form_field">
             <label for="" class="name_form_field">Email : </label>
-            <input type="text" class="textfile" id="email" name="EMAIL" style="width: 400px;">
+            <input type="text" class="textfile" id="email" name="email" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div class="form_field">
             <label for="" class="name_form_field">Ngày sinh : </label>
-            <input type="date" class="textfile" id="birthDay" name="NGAYSINH" style="width: 400px;">
+            <input type="date" class="textfile" id="birthDay" name="ngaySinh" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div class="form_field">
             <label for="" class="name_form_field">Tên đăng nhập : </label>
-            <input type="text" class="textfile" id="userName" name="TENDNKH" style="width: 400px;">
+            <input type="text" class="textfile" id="userName" name="tendn" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div class="form_field">
             <label for="" class="name_form_field">Mật khẩu : </label>
-            <input type="password" class="textfile" id="password" name="MATKHAUKH" style="width: 400px;">
-            <span class="error_message"></span>
-        </div>
-        <div class="form_field">
-            <label for="" class="name_form_field">Xác nhận lại mật khẩu : </label>
-            <input type="password" class="textfile" id="password_confirmation" style="width: 400px;">
+            <input type="password" class="textfile" id="password" name="matKhau" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div style="display: flex; justify-content: space-between; width: 400px;">
             <div class="form_field">
                 <label for="" class="name_form_field">Tỉnh : </label>
-                <select class="textfile" name="MATINH" style="width: 195px;" id="Tinh">
+                <select id='provinces' onchange='getProvinces(event)' class="textfile" name="tinh" style="width: 195px;"
+                    required>
                     <option value="">Chọn tỉnh/Thành phố</option>
-                    <option value="@item.MATINH"></option>
+
                 </select>
                 <span class="error_message"></span>
             </div>
-
             <div class="form_field">
                 <label for="" class="name_form_field">Huyện : </label>
-                <select class="textfile" name="MAHUYEN" style="width: 195px;" id="Huyen" disabled>
-
-                    <option value="">Chọn huyện</option>
+                <select id='districts' onchange='getDistricts(event)' class="textfile" name="huyen"
+                    style="width: 195px;" id="Huyen">
+                    <option value="">Chọn Huyện</option>
                 </select>
                 <span class="error_message"></span>
             </div>
@@ -64,16 +172,14 @@
         <div style="display: flex; justify-content: space-between; width: 400px;">
             <div class="form_field">
                 <label for="" class="name_form_field">Xã : </label>
-                <select class="textfile" name="MAXA" style="width: 195px;" id="Xa" disabled>
-                    <option value="">Chọn xã</option>
+                <select id="wards" class="textfile" name="MAXA" style="width: 195px;" id="Xa">
+                    <option value="">Chọn Xã</option>
                 </select>
                 <span class="error_message"></span>
             </div>
-
             <div class="form_field">
-                <label for="" class="name_form_field">Số nhà và tên đường : </label>
-                <input type="text" class="textfile" style="width: 195px;" id="address" name="DAICHI">
-                <span class="error_message"></span>
+                <label for="" class="name_form_field">Số nhà : </label>
+                <input type="diaChi" class="textfile" id="diaChi" name="diaChi" style="width: 195px;" required>
             </div>
         </div>
 
@@ -91,7 +197,7 @@
             </div>
         </div>
         <div class="button">
-            <input type="submit" value="Đăng ký" class="button_add_admin" />
+            <input type="submit" name="submit" value="Đăng ký" class="button_add_admin" />
         </div>
     </form>
 </div>
