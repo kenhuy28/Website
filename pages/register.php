770b492da0ab739535ada159148c22964876f1c1
8ivlvi8 <letam2002pk@gmail.com>
2023-11-02 21:23:58 +0700

    đang xử lý xã

--- pages/register.php
+++ pages/register.php
@@ -1,71 +1,10 @@
-<!--register-->
-
 <?php
 include '../templates/header.php';
-$conn = mysqli_connect('localhost', 'root', '', 'qlpkthucung');
-mysqli_set_charset($conn, 'UTF8');
-
-$maKhachHanh = "KH";
-$query = "SELECT COUNT(*) FROM `khach_hang`";
-$statement = $dbh->prepare($query);
+$get_tinh = "SELECT `maTinh`, `tenTinh` FROM `tinh`";
+$statement = $dbh->prepare($get_tinh);
 $statement->execute();
-// Sử dụng fetchColumn
-$count = $statement->fetchColumn() + 1;
-if ($count <= 9) {
-    $maKhachHanh .= "000" . $count;
-} else if ($count <= 99) {
-    $maKhachHanh .= "00" . $count;
-} else if ($count <= 999) {
-    $maKhachHanh .= "0" . $count;
-} else {
-    $maKhachHanh .= "" . $count;
-}
+$statement->setFetchMode(PDO::FETCH_OBJ);
 
-if (!empty($_POST['hoKhachHang'])) {
-    $hoKhachHang = $_POST['hoKhachHang'];
-} else
-    $hoKhachHang = "";
-if (!empty($_POST['tenKhachHang'])) {
-    $tenKhachHang = $_POST['tenKhachHang'];
-} else
-    $tenKhachHang = "";
-if (!empty($_POST['dienThoai'])) {
-    $dienThoai = $_POST['dienThoai'];
-} else
-    $dienThoai = "";
-if (!empty($_POST['diaChi'])) {
-    $diaChi = $_POST['diaChi'];
-} else
-    $diaChi = "";
-if (!empty($_POST['tendn'])) {
-    $tendn = $_POST['tendn'];
-} else
-    $tendn = "";
-if (!empty($_POST['matKhau'])) {
-    $matKhau = $_POST['matKhau'];
-} else
-    $matKhau = "";
-if (!empty($_POST['email'])) {
-    $email = $_POST['email'];
-} else
-    $email = "";
-if (!empty($_POST['ngaySinh'])) {
-    $ngaySinh = $_POST['ngaySinh'];
-} else
-    $ngaySinh = "";
-if (!empty($_GET["tinh"])) {
-    $tinh = $_POST["tinh"];
-} else
-    $tinh = "";
-$fileUpLoad = $tendn . ".jpg";
-$khoiPhuc = "123456";
-$maXa = "X00011";
-if (isset($_POST["submit"])) {
-
-    $sql_themKhachHang = "INSERT INTO khach_hang VALUES ('$maKhachHanh','$hoKhachHang','$tenKhachHang','$dienThoai','$diaChi','$tendn','$matKhau','$email','$ngaySinh','$fileUpLoad','$khoiPhuc','$maXa');";
-    $rerult_themKhachHang = mysqli_query($conn, $sql_themKhachHang);
-    echo $tinh;
-}
 ?>
 
 <h6>Trang chủ > Đăng ký </h6>
@@ -120,56 +59,57 @@ if (isset($_POST["submit"])) {
 <div class="create_admin">
     <h1 class="Title_Admin_create_form">Tạo tài khoản </h1>
     <p class="Notification_create_form">Vui lòng điền thông tin bên dưới</p>
-    <form class="create_admin_form" method="POST">
+    <form class="create_admin_form" action="../includes/register.php" method="POST">
         <div class="form_field">
-            <label for="" class="name_form_field">Họ khách hàng: </label>
+            <label for="" class="name_form_field">Họ: </label>
             <input type="text" class="textfile" name="hoKhachHang" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div class="form_field">
-            <label for="" class="name_form_field">Tên khách hàng: </label>
+            <label for="" class="name_form_field">Tên: </label>
             <input type="text" class="textfile" name="tenKhachHang" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div class="form_field">
-            <label for="" class="name_form_field">Số điện thoại : </label>
+            <label for="" class="name_form_field">Số điện thoại: </label>
             <input type="text" class="textfile" id="phoneNumber" name="dienThoai" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div class="form_field">
-            <label for="" class="name_form_field">Email : </label>
+            <label for="" class="name_form_field">Email: </label>
             <input type="text" class="textfile" id="email" name="email" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div class="form_field">
-            <label for="" class="name_form_field">Ngày sinh : </label>
+            <label for="" class="name_form_field">Ngày sinh: </label>
             <input type="date" class="textfile" id="birthDay" name="ngaySinh" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div class="form_field">
-            <label for="" class="name_form_field">Tên đăng nhập : </label>
+            <label for="" class="name_form_field">Tên đăng nhập: </label>
             <input type="text" class="textfile" id="userName" name="tendn" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div class="form_field">
-            <label for="" class="name_form_field">Mật khẩu : </label>
+            <label for="" class="name_form_field">Mật khẩu: </label>
             <input type="password" class="textfile" id="password" name="matKhau" style="width: 400px;" required>
             <span class="error_message"></span>
         </div>
         <div style="display: flex; justify-content: space-between; width: 400px;">
             <div class="form_field">
-                <label for="" class="name_form_field">Tỉnh : </label>
-                <select id='provinces' onchange='getProvinces(event)' class="textfile" name="tinh" style="width: 195px;"
-                    required>
-                    <option value="">Chọn tỉnh/Thành phố</option>
-
+                <label for="" class="name_form_field">Tỉnh: </label>
+                <select id='provinces' class="textfile" name="tinh" style="width: 195px;">
+                    <option value="" disabled selected>Chọn tỉnh/Thành phố</option>
+                    <?php
+                    while ($row = $statement->fetch())  
+                        echo "<option value='{$row->maTinh}'>{$row->tenTinh}</option>";
+                    ?>
                 </select>
                 <span class="error_message"></span>
             </div>
             <div class="form_field">
-                <label for="" class="name_form_field">Huyện : </label>
-                <select id='districts' onchange='getDistricts(event)' class="textfile" name="huyen"
-                    style="width: 195px;" id="Huyen">
+                <label for="" class="name_form_field">Huyện: </label>
+                <select id='districts' class="textfile" name="huyen" style="width: 195px;" id="Huyen">
                     <option value="">Chọn Huyện</option>
                 </select>
                 <span class="error_message"></span>
@@ -177,19 +117,19 @@ if (isset($_POST["submit"])) {
         </div>
         <div style="display: flex; justify-content: space-between; width: 400px;">
             <div class="form_field">
-                <label for="" class="name_form_field">Xã : </label>
-                <select id="wards" class="textfile" name="MAXA" style="width: 195px;" id="Xa">
+                <label for="" class="name_form_field">Xã: </label>
+                <select id="wards" class="textfile" name="maXa" style="width: 195px;" id="Xa">
                     <option value="">Chọn Xã</option>
                 </select>
                 <span class="error_message"></span>
             </div>
             <div class="form_field">
-                <label for="" class="name_form_field">Số nhà : </label>
+                <label for="" class="name_form_field">Chi tiết: </label>
                 <input type="diaChi" class="textfile" id="diaChi" name="diaChi" style="width: 195px;" required>
             </div>
         </div>
 
-        <div class="form_field" style="max-width: 400px">
+        <!-- <div class="form_field" style="max-width: 400px">
             <label for="" class="name_form_field">Ảnh đại diện : </label>
             <div class="custom-file">
                 <div class="form_field">
@@ -201,7 +141,7 @@ if (isset($_POST["submit"])) {
                     <img src="" alt="" id="custom-file-img-display">
                 </div>
             </div>
-        </div>
+        </div> -->
         <div class="button">
             <input type="submit" name="submit" value="Đăng ký" class="button_add_admin" />
         </div>
