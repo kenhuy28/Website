00bc3dd1baf2194b3ff90dd6f6e0d7742e034dc1
Lam Ngô <ngotuanlam147@hotmail.com>
2023-11-06 23:49:57 +0700

    chuaxongdau

--- pages/product_detail_page.php
+++ pages/product_detail_page.php
@@ -1,26 +1,72 @@
 <!--peoduct detail-->
 <?php include '../templates/header.php';
-
-$query = "SELECT maSanPham, tenSanPham, donGiaBan, maLoai, soLuong, hinhAnh, moTa, tenThuongHieu FROM san_pham JOIN thuong_hieu on san_pham.maThuongHieu = thuong_hieu.maThuongHieu WHERE maSanPham= 'sp0001'";
+$product_id = $_POST['maSanPham'];
+$query = "SELECT maSanPham, tenSanPham, donGiaBan, maLoai, soLuong, hinhAnh, moTa, tenThuongHieu FROM san_pham JOIN thuong_hieu on san_pham.maThuongHieu = thuong_hieu.maThuongHieu WHERE maSanPham= '$product_id'";
 $statement = $dbh->prepare($query);
 $statement->execute();
 $statement->setFetchMode(PDO::FETCH_OBJ);
-if (isset($_POST["submit"])) {
-}
 
 ?>
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    function addToCart(element) {
+        var maSanPham = element.getAttribute("productid");
+        var userID = '<?php echo $user_id; ?>';
+        excuteToCart(maSanPham, userID);
+    }
+    function excuteToCart(maSanPham, nguoiDung) {
+        jQuery.ajax({
+            url: '<?php echo $rootPath . "/includes/update_cart.php"; ?>', // Đường dẫn đến tệp xử lý PHP trên máy chủ
+            type: 'POST',
+            data: { maSanPham: maSanPham, nguoiDung: nguoiDung }, // Gửi ID sản phẩm lên máy chủ
+            dataType: 'json',
+            success: function (response) {
+                var TB = document.querySelector('#notifacation_all');
+                TB.style.bottom = "30px";
+                setTimeout(function () {
+                    TB.style.bottom = "-50px";
+                }, 2000);
+                console.log(response);
+                updateGioHang(maSanPham, nguoiDung);
+            },
+            error: function (xhr, status, error) {
+                // Xử lý lỗi (nếu có)
+                console.log(error);
+            }
+        });
+    }
+    function updateGioHang(maSanPham, nguoiDung) {
+        jQuery.ajax({
+            url: '<?php echo $rootPath . "/includes/count_gio_hang.php"; ?>', // Đường dẫn đến tệp xử lý PHP trên máy chủ
+            type: 'POST',
+            data: { maSanPham: maSanPham, nguoiDung: nguoiDung }, // Gửi ID sản phẩm lên máy chủ
+            dataType: 'json',
+            success: function (response) {
+                var count = document.querySelector('#count_cart');
+                count.textContent = response['soLuongTG'];
+                console.log(response);
+            },
+            error: function (xhr, status, error) {
+                // Xử lý lỗi (nếu có)
+                console.log(error);
+            }
+        });
+    }
+</script>
 <div class="chiTietSanPham">
+
     <?php
+
     while ($row = $statement->fetch()) {
+        $maSanPham = $row->maSanPham;
+        $soLuong = $row->soLuong;
         if ($row->soLuong != 0) {
-            $button = '<input type="submit" name="submit" class="button_product_chiTiet" style="font-size:20px; color:red; font-weight:bold;"
+            $button = '<input type="submit" productid=' . $maSanPham . ' onclick="addToCart(this)" name="submit" class="button_product_chiTiet" style="font-size:20px; color:red; font-weight:bold;"
             value="THÊM VÀO GIỎ HÀNG">';
         } else {
             $button = '<input type="submit" name="submit" style="font-size:20px; color:red; font-weight:bold;" value="HẾT HÀNG" disabled>';
         }
-        $maSanPham = $row->maSanPham;
-        $soLuong = $row->soLuong;
+
         echo '
 
         <div class="chiTietSanPham_left">
@@ -43,9 +89,9 @@ if (isset($_POST["submit"])) {
 
                     </h3>
                 </div>
-                <form acction=""  method="POST">
+
                 ' . $button . '
-                </form>
+
                 <div class="chiTietSanPham_right_mota">
                     <span>Mô tả: </span>
                     ' . $row->moTa . '
@@ -53,8 +99,5 @@ if (isset($_POST["submit"])) {
         </div>';
     }
     ?>
-    <style>
-        .submit
-    </style>
 </div>
 <?php include '../templates/footer.php' ?>
