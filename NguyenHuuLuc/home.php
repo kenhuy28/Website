c56929e4cb6825c026f2a02ba1b514c32f27903c
huuluc10 <lucnguyenhuu91@gmail.com>
2023-11-07 23:13:18 +0700

    Xong bt form

---
+++ NguyenHuuLuc/home.php
@@ -0,0 +1,37 @@
+<?php
+include '../templates/header.php';
+?>
+<style>
+    /* li {
+        color: black;
+    } */
+    .bt {
+        color: blue;
+    }
+</style>
+<h2>Bài tập form</h2>
+<ul>
+    <li>
+        <a href="tinhDienTichHCN.php">
+            <p class="bt">Tính diện tích hình chữ nhật</p>
+        </a>
+    </li>
+    <li>
+        <a href="tinhTienDien.php">
+            <p class="bt">Tính tiền điện</p>
+        </a>
+    </li>
+    <li>
+        <a href="phepTinh.php">
+            <p class="bt">Thực hiện phép tính</p>
+        </a>
+    </li>
+    <li>
+        <a href="nhapThongtin.php">
+            <p class="bt">Nhập thông tin</p>
+        </a>
+    </li>
+</ul>
+<br>
+<h2>Bài tập mảng</h2>
+<?php include '../templates/footer.php' ?>
---
+++ NguyenHuuLuc/phepTinh.php
@@ -0,0 +1,57 @@
+<?php include '../templates/header.php'; ?>
+
+<style>
+    label {
+        color: red;
+    }
+</style>
+
+<form action="" method="post">
+    <table>
+        <thead align="center">
+            <th>
+                <h3 style="color: blue;">PHÉP TÍNH TRÊN HAI SỐ</h3>
+            </th>
+        </thead>
+        <tbody>
+            <tr>
+                <td style="color: red;">Chọn phép tính: </td>
+                <td>
+                    <input type="radio" name="phepTinh" value="+" checked> <label>Cộng</label>
+                    <input type="radio" name="phepTinh" value="-"> <label>Trừ</label>
+                    <input type="radio" name="phepTinh" value="*"> <label>Nhân</label>
+                    <input type="radio" name="phepTinh" value="/"> <label>Chia</label>
+                </td>
+            </tr>
+            <tr>
+                <td style="color:blue;">Số thứ nhất:</td>
+                <td>
+                    <input type="text" name="so1">
+                </td>
+            </tr>
+            <tr>
+                <td style="color:blue;">Số thứ hai:</td>
+                <td>
+                    <input type="text" name="so2">
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="Tính" name="tinh"></td>
+            </tr>
+        </tbody>
+    </table>
+</form>
+
+<?php if (isset($_POST["tinh"])) {
+    if (!isset($_POST["so1"], $_POST["so2"])) {
+        echo "<script>alert(\"Nhập thiếu số\");</script>";
+    } else {
+        $phepTinh = $_POST["phepTinh"];
+        $so1 = $_POST["so1"];
+        $so2 = $_POST["so2"];
+        echo "<script>window.location=\"ketquapheptinh.php?phepTinh=" . $phepTinh . "&so1=" . $so1 . "&so2=" . $so2 . "\"</script>";
+    }
+} ?>
+
+<?php include '../templates/footer.php' ?>
