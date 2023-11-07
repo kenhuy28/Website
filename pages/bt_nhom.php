d88b02b095ada2e2cc7a2bc3b2ef32d9da2899fe
huuluc10 <lucnguyenhuu91@gmail.com>
2023-11-07 20:27:44 +0700

    tạo layout cho trang bt nhóm

--- pages/bt_nhom.php
+++ pages/bt_nhom.php
@@ -1,11 +1,18 @@
 <?php include '../templates/header.php' ?>
+<style>
+
+</style>
 <div style="display:flex; justify-content:center; align-items:center; min-height: 400px">
-    <h1>Bài tập nhóm</h1>
-    <ul>
-        <li><a>Nguyễn Hữu Lực</a></li>
-        <li><a>Nguyễn Lê Tâm</a></li>
-        <li><a>Phạm Phương Nam</a></li>
-        <li><a>Ngô Tuấn Lamc</a></li>
-    </ul>
+    <a href="<?php echo $rootPath . "/btNhom/NguyenHuuLuc/" ?>" style="width: 25%;">
+        <div align="center">
+            <img src=" <?php echo $rootPath . "/assets/img/thanh_vien/huuluc.jpg" ?>" width="80%">
+
+            <p style="color:blue;">Nguyễn Hữu Lực - 62133901</p>
+        </div>
+    </a>
+
+    <div style="width: 25%" align="center">Nguyễn Lê Tâm</div>
+    <div style="width: 25%" align="center">Phạm Phương Nam</div>
+    <div style="width: 25%" align="center">Ngô Tuấn Lam</div>
 </div>
 <?php include '../templates/footer.php' ?>
