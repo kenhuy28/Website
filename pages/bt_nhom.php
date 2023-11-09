10acbfcf3c9d2973397af8912f0367b580feb4c4
8ivlvi8 <letam2002pk@gmail.com>
2023-11-09 10:40:10 +0700

    edit

--- pages/bt_nhom.php
+++ pages/bt_nhom.php
@@ -10,8 +10,12 @@
             <p style="color:blue;">Nguyễn Hữu Lực - 62133901</p>
         </div>
     </a>
-
-    <div style="width: 25%" align="center">Nguyễn Lê Tâm</div>
+    <a href="<?php echo $rootPath . "/NguyenLeTam/" ?>" style="width: 25%;">
+        <div align="center">
+            <img src=" <?php echo $rootPath . "/assets/img/thanh_vien/letam.png" ?>" width="80%">
+            <p style="color:blue;">Nguyễn Lê Tâm - 62134195</p>
+        </div>
+    </a>
     <div style="width: 25%" align="center">Phạm Phương Nam</div>
     <div style="width: 25%" align="center">Ngô Tuấn Lam</div>
 </div>
